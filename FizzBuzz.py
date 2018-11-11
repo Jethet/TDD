9f38a3740c97283ee153e29bed6916b32ccd3514
jethet <henriette.hettinga@gmail.com>
2018-11-11 17:17:02 +0100

    Write code for testing

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,10 +1,40 @@
+# code should enable input of a range of values instead of one
+
 import unittest
 
-def fizzbuzz():
-    pass
+def fizzbuzz(x):
+
+    if x % 3 == 0 and x % 5 == 0:
+        #print("FizzBuzz")
+        return "FizzBuzz"
+
+    elif x % 3 == 0:
+        #print("Fizz")
+        return "Fizz"
+
+    elif x % 5 == 0:
+        #print("Buzz")
+        return "Buzz"
+
+    else:
+        print(x)
+
 
 class Test_Fizzbuzz(unittest.TestCase):
     def setUp(self):
         pass
 
-        
+    def test_Fizz(self):
+        expected = "Fizz"
+        self.assertEqual(expected, fizzbuzz(3))
+
+    def test_Buzz(self):
+        expected = "Buzz"
+        self.assertEqual(expected, fizzbuzz(5))
+
+    def test_FizzBuzz(self):
+        expected = "FizzBuzz"
+        self.assertEqual(expected, fizzbuzz(15))
+
+if __name__ == '__main__':
+    unittest.main()

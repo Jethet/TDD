1f03a280998174433917b96a70b57cbf37eaaa12
jethet <henriette.hettinga@gmail.com>
2019-02-12 18:01:30 +0100

    Use unittest on countRange.py code

--- countRange.py
+++ countRange.py
@@ -1,40 +1,28 @@
 """
 Python’s standard library includes a method named count that determines how
-many times a specific value occurs in a list. In this exercise, you will create a new
-function named countRange which determines and returns the number of elements
-within a list that are greater than or equal to some minimum value and less than some
-maximum value. Your function will take three parameters: the list, the minimum
-value and the maximum value. It will return an integer result greater than or equal to
-0. Include a main program that demonstrates your function for several different lists,
-minimum values and maximum values. Ensure that your program works correctly
-for both lists of integers and lists of floating point numbers.
+many times a specific value occurs in a list. In this exercise, you will create
+a new function named countRange which determines and returns the number of
+elements within a list that are greater than or equal to some minimum value
+and less than some maximum value. Your function will take three parameters:
+the list, the minimum value and the maximum value. It will return an integer
+result greater than or equal to 0. Include a main program that demonstrates
+your function for several different lists, minimum values and maximum values.
+Ensure that your program works correctly for both lists of integers and
+lists of floating point numbers.
 
 """
-#import unittest
 
 def countRange(mylist, min_val, max_val):
     list_values = []
-    min_val = 0
-    max_val = 0
+    #min_val = 0
+    #max_val = 0
     count = 0
     for num in mylist:
         if num >= min_val and num < max_val:
             list_values.append(num)
-            count = list_values.count(num)
-            return count
+            print(list_values)
+            #count = list_values.count(num)
+        #return count
 
 
-print(countRange([2, 14, 23, 3], 2, 26))
-
-
-
-class Test_countRange(unittest.TestCase):
-    def setUp(self):
-        pass
-
-    def test_countMin(self):
-        expected = myList(num >= x and num< y)
-        self.assertEqual(expected, mylist(num))
-
-if __name__ == '__main__':
-    unittest.main()
+#print(countRange([2, 14, 23, 3], 2, 26))
---
+++ countRange_test.py
@@ -0,0 +1,9 @@
+import unittest
+import countRange
+
+class TestCountRange(unittest.TestCase):
+
+    def test_first_list(self):
+        mylist = [2, 14, 23, 3]
+        min_val = 2
+        max_val = 26

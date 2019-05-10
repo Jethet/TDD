50819da46122a32c94be669ca31c0811d350f1e7
Jethet <henriette.hettinga@gmail.com>
2019-05-10 13:01:31 +0200

    Test code with error

--- test_sum.py
+++ test_sum.py
@@ -6,12 +6,12 @@
 #  - what happens when one of the values is negative?
 
 # Structure of the test:
-#  1. Create inputs
+#  1. Create inputs (it is common to reuse them)
 #  2. Execute code being tested, capturing the output
 #  3. Compare the output with the expected result
 
 import unittest
-
+from fractions import Fraction
 from my_sum import get_sum
 
 class TestSum(unittest.TestCase):
@@ -20,6 +20,12 @@ class TestSum(unittest.TestCase):
         data = [1,2,3]
         result = sum(data)
         self.assertEqual(result, 6)
+
+    def test_list_fraction(self):
+        # This tests if get_sum() can sum a list of fractions:
+        data = [Fraction(1,4), Fraction(1,4), Fraction(2,5)]
+        result = sum(data)
+        self.assertEqual(result, 1)
 
 if __name__ == '__main__':
     unittest.main()
